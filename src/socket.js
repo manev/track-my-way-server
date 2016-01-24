25527a5a7cb17c18681c86fbcd3f1c636e4aab07
kaloyan manev <kokomanev84@gmail.com>
2016-01-24 13:12:48 +0200

    introduced mongodb

--- src/socket.js
+++ src/socket.js
@@ -10,9 +10,15 @@ function initializeWebSocket() {
 	var MongoClient = require('mongodb').MongoClient;
 
 	var url = 'mongodb://localhost:27017/users';
+	if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+		url = 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+					process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+				  	process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+					process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+					process.env.OPENSHIFT_APP_NAME;
+	}
 
 	io.sockets.on('connection', function (socket) {
-
 		socket.on('android', function (data) {
 	    	io.emit('send-wp', data);
 	    });
@@ -21,13 +27,43 @@ function initializeWebSocket() {
 	    	io.emit('send-android', data);
 	    });
 
-	    if(socket.handshake.query.number){
-	    	socket.on(socket.handshake.query.number, function(userSender){
-	    		io.emit("request-track-result", userSender);
-	    	});
-	    }
+	    socket.on("request-user-track", function(target){
+			var targetUser = JSON.parse(target);
+
+	    	var senderRoom = socket.user.Phone.Number + "-send-position-event";
+	    	socket.join(senderRoom);
+
+	    	var requestUserEvent = targetUser.Phone.Number + "-request-user-event";
+	    	io.in(requestUserEvent).emit(requestUserEvent, JSON.stringify(socket.user));
+	    });
+
+	    socket.on("request-user-track-result", function(target){
+			var targetUser = JSON.parse(target);
+
+	    	if(targetUser.IsAccepted){
+	    		var room = socket.user.Phone.Number + "-send-position-event";
+	    		socket.join(room);
+	    	}
+
+	    	var room = targetUser.SenderUser.Phone.Number + "-request-user-event";
+
+	    	targetUser.SenderUser = socket.user;
+	    	io.in(room).emit("request-user-event-result", JSON.stringify(targetUser));
+	    });
+
+		socket.on("send-position-event", function(target, position){
+			var targetUser = JSON.parse(target);
+	    	var room = targetUser.Phone.Number + "-send-position-event";
+	    	io.in(room).emit("send-position-event", position);
+		});
 
 	    socket.on('loggin-user-event', function(data){
+	    	var user = JSON.parse(data);
+	    	var requestUserEvent = user.Phone.Number + "-request-user-event";
+	    	socket.join(requestUserEvent);
+
+	    	socket.user = user;
+
 	    	MongoClient.connect(url, function(err, db) {
 	    		if(err) {
 					console.log(err);
