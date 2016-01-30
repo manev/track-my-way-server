c5daca96bfae108eacbdf30682b3fc0b9ceed1b5
Kaloyan Manev <Kaloyan Manev>
2016-01-30 18:42:33 +0200

    a

--- src/socket.js
+++ src/socket.js
@@ -102,7 +102,7 @@ function initializeWebSocket() {
     		io.emit('log', "add-user-event");
 	    	mongoOp(function(db){
     			io.emit('log', "before insert into users");
-    			io.emit('log', JSON.stringify(db.collection));
+    			io.emit('log', JSON.stringify(db));
 	    		db.collection('users').insertOne(JSON.parse(data), function(err, result){
     				io.emit('log', "insert user");
 					if(err) {
