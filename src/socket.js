9fef627ad2d8c89e9761e2e1601d620513de766a
manev <koko_manew@yahoo.com>
2017-02-01 15:09:42 +0200

    WIP

--- src/socket.js
+++ src/socket.js
@@ -12,7 +12,7 @@ function initializeWebSocket() {
     var gcm = require('node-gcm');
 
     var user = "admin";
-    var pass = "admin";
+    var pass = "aC4yFiqqu6zY";
 
     var userSocketsMap = {};
 
